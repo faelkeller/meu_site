bf0d69c39d2c88025ff999133309744a9ae33c61
Rafael Keller <rafaelkeller@sisconsultoria.com.br>
2020-04-28 11:12:43 -0300

    Habilitando o multisite para o site em inglês

--- wp-config.php
+++ wp-config.php
@@ -2,6 +2,17 @@
 
 include '.env.php';
 
+define('FS_METHOD','direct');
+define( 'WP_ALLOW_MULTISITE', true );
+
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', true);
+define('DOMAIN_CURRENT_SITE', 'local.meusite');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
+
+
 /* Isto é tudo, pode parar de editar! :) */
 
 /** Caminho absoluto para o diretório WordPress. */
@@ -11,5 +22,5 @@ if ( !defined('ABSPATH') )
 /** Configura as variáveis do WordPress e arquivos inclusos. */
 require_once(ABSPATH . 'wp-settings.php');
 
-define('FS_METHOD','direct');
-define( 'WP_ALLOW_MULTISITE', true );
+
+
